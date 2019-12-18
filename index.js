ca791401341e48093a2439c95febdc61ea09e8dd
Nana <abekah12345@gmail.com>
2019-12-17 20:49:54 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,127 @@
+const express = require('express')
+const pg = require('pg')
+require('dotenv').config();
+const app = express();
+const rateLimiter = require('./rateLimiter');
+
+// configs come from standard PostgreSQL env vars
+// https://www.postgresql.org/docs/9.6/static/libpq-envars.html
+
+app.use(rateLimiter);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('user', 'Nana');
+  next();
+});
+
+const pool = new pg.Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT
+});
+
+const queryHandler = (req, res, next) => {
+  pool.query(req.sqlQuery).then((r) => {
+    return res.json(r.rows || [])
+  }).catch(next)
+}
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to EQ Works 😎')
+})
+
+app.get('/events', (req, res, next) => {
+  req.sqlQuery = `
+    SELECT *
+    FROM public.hourly_events
+    ORDER BY date, hour
+    LIMIT 168;
+  `
+  return next()
+}, queryHandler)
+
+app.get('/events/hourly', (req, res, next) => {
+  req.sqlQuery = `
+    SELECT date, hour, events
+    FROM public.hourly_events
+    ORDER BY date, hour
+    LIMIT 168;
+  `
+  return next()
+}, queryHandler);
+
+
+app.get('/events/daily', (req, res, next) => {
+  req.sqlQuery = `
+    SELECT date, SUM(events) AS events
+    FROM public.hourly_events
+    GROUP BY date
+    ORDER BY date
+    LIMIT 7;
+  `
+  return next()
+}, queryHandler)
+
+app.get('/stats', (req, res, next) => {
+  req.sqlQuery = `
+    SELECT *
+    FROM public.hourly_stats
+    ORDER BY date, hour
+    LIMIT 168;
+  `
+  return next()
+}, queryHandler)
+
+app.get('/stats/hourly', (req, res, next) => {
+  req.sqlQuery = `
+    SELECT date, hour, impressions, clicks, revenue
+    FROM public.hourly_stats
+    ORDER BY date, hour
+    LIMIT 168;
+  `
+  return next()
+}, queryHandler)
+
+app.get('/stats/daily', (req, res, next) => {
+  req.sqlQuery = `
+    SELECT date,
+        SUM(impressions) AS impressions,
+        SUM(clicks) AS clicks,
+        SUM(revenue) AS revenue
+    FROM public.hourly_stats
+    GROUP BY date
+    ORDER BY date
+    LIMIT 7;
+  `
+  return next()
+}, queryHandler)
+
+app.get('/poi', (req, res, next) => {
+  req.sqlQuery = `
+    SELECT *
+    FROM public.poi;
+  `
+  return next()
+}, queryHandler)
+
+app.listen(process.env.PORT || 5555, (err) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  } else {
+    console.log(`Running on ${process.env.PORT || 5555}`)
+  }
+})
+
+// last resorts
+process.on('uncaughtException', (err) => {
+  console.log(`Caught exception: ${err}`)
+  process.exit(1)
+})
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason)
+  process.exit(1)
+})
---
+++ rateLimiter.js
@@ -0,0 +1,37 @@
+const redis = require('redis');
+const redisClient = redis.createClient();
+const moment = require('moment');
+
+const MAX_REQ = 1000;
+
+module.exports = (req,res,next) => {
+    redisClient.exists(req.headers.user, (err, response) => {
+        if(err){
+            console.log("Redis is out........");
+            system.exit(0);
+        }
+        if(response === 1){
+            redisClient.get(req.headers.user, (err, response) => {
+                let data = JSON.parse(response);
+                let currentTime = moment().unix();
+                let diff = (currentTime - data.startTime)/60;
+
+                if(diff >= 1){
+                    if(data.count > MAX_REQ){
+                        return res.json({ "error": 1, "message": "API limit exceeded" });
+                    }
+                    data.count++;
+                    redisClient.set(req.headers.user, JSON.stringify(data));
+                    next();
+                }
+            });
+        } else {
+            let body = {
+                "count" : 1,
+                "startTime" : moment().unix()
+            }
+            redisClient.set(req.headers.user, JSON.stringify(body));
+            next()
+        }
+    });
+};
